78e57bfe32bed3fecc779f3f080b7e68ae7343c4
im2187 <im2187>
2024-01-28 15:19:01 +0900

    3.5

--- src/server.js
+++ src/server.js
@@ -4,15 +4,30 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("<h1>I still love you</h1>");
-};
-const handleLogin = (req, res) => {
-  return res.send({ message: "Login here." });
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 };
 
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("I love middleware");
+};
+
+const handleProtected=(req,res)=>{
+  
+}
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
